11f6343db1a3591f1eb926ce2c723444e21dca89
Joel Shaw <shawji@me.com>
2022-05-24 21:13:59 -0500

    created header, list component & routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './list/create/create.component';
+import { ListComponent } from './list/list.component';
+
+const routes: Routes = [
+  {
+    path: 'list',
+    component: ListComponent
+  },
+  {
+    path: 'list/new',
+    component: CreateComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
