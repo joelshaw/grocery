b9e5bf1feebdcde245c9fd076cf14f9cb8ac4691
Joel Shaw <shawji@me.com>
2022-05-29 21:57:58 -0500

    Added grocery list and expiring list to homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ListComponent } from '../list/list.component';
+import { ExpireComponent } from '../expire/expire.component';
+
 @Component({
   selector: 'grocery-home',
   templateUrl: './home.component.html',
