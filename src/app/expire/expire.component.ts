46e6da6b2caa278fba7b2c64cf1ecd3bf558a109
Joel Shaw <shawji@me.com>
2022-05-29 21:57:19 -0500

    Create expire component

---
+++ src/app/expire/expire.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'grocery-expire',
+  templateUrl: './expire.component.html',
+  styleUrls: ['./expire.component.scss']
+})
+export class ExpireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
